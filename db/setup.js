cb309ad756488ce3fd48814d397278f931a5507b
JackVCurtis <jack.curtis@elasticm2m.com>
2019-02-05 09:45:08 -0500

    add package.json and db setup script

---
+++ db/setup.js
@@ -0,0 +1,39 @@
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+console.log("testing db connection");
+console.log(process.env.DATABASE_URL);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+});
+
+pool.on('connect', () => {
+  console.log('connected to the db');
+});
+
+pool.on('remove', () => {
+  console.log('client removed');
+  process.exit(0);
+});
+
+const queryText =
+  `CREATE TABLE IF NOT EXISTS
+    accounts(
+      id UUID PRIMARY KEY,
+      password VARCHAR(128),
+      email VARCHAR(128),
+      created_date TIMESTAMP,
+      modified_date TIMESTAMP
+    )`;
+
+pool.query(queryText)
+  .then((res) => {
+    console.log(res);
+    pool.end();
+  })
+  .catch((err) => {
+    console.log(err);
+    pool.end();
+  });
