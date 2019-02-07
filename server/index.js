8fba5bb5713776e8c1968cf52f6af7bd232b32ee
JackVCurtis <jack.curtis@elasticm2m.com>
2019-02-07 12:23:25 -0500

    add webpack

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const expressJwt = require('express-jwt');
 const dotenv = require('dotenv');
+const path = require('path');
 
 const accountRouter = require('./routers/accounts');
 const loginRouter = require('./routers/login');
@@ -10,6 +11,8 @@ const app = express();
 dotenv.config();
 
 app.use(bodyParser.json());
+
+app.use('/static', express.static(path.join(__dirname, 'static')));
 app.use('/api', expressJwt({secret: process.env.JWT_SECRET}).unless({path: [
     '/api/login',
     { url: '/api/accounts', methods: ['POST'] }
@@ -18,7 +21,7 @@ app.use('/api', loginRouter);
 app.use('/api', accountRouter);
 
 app.use('/*', function(req, res) {
-    res.sendFile('index.html', {root: __dirname});
+    res.sendFile('/static/index.html', {root: __dirname});
 });
 
 app.listen(3000, () => console.log('Form and Function listening on port 3000'));
