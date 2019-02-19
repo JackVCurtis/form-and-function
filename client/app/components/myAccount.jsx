8abe6dc93985387f9ca0a27441a275ffc20a97ad
JackVCurtis <jack.curtis@elasticm2m.com>
2019-02-19 10:30:35 -0500

    add My Account placeholder

---
+++ client/app/components/myAccount.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class MyAccount extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (<h2>My Account</h2>);
+    }
+}
